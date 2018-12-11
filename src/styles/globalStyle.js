7902e5c3bc584408e223f38e463337a17e68c2d0
Taesu Hyeon <qvil1127@gmail.com>
2018-12-12 00:14:51 +0900

    Add Global Style

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  #root{
+    width: 300px;
+    height: 200px;
+  }
+`;
+
+export default GlobalStyle;
