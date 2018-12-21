d252a90fe2fd9adde14bf1501e5c019a721ac465
Taesu Hyeon <qvil1127@gmail.com>
2018-12-22 02:38:24 +0900

    How do i set async init state??

--- src/graphql/clientState.js
+++ src/graphql/clientState.js
@@ -1,17 +1,19 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { withClientState } from "apollo-link-state";
-import resolvers, { defaults } from "./resolvers";
+// import resolvers, { defaults } from "./resolvers";
+import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
+import { getTodos } from "../chrome";
 
 const cache = new InMemoryCache();
-const stateLink = withClientState({ cache, typeDefs, resolvers, defaults });
+const stateLink = withClientState({ cache, typeDefs, resolvers });
 const client = new ApolloClient({
   cache,
-  link: stateLink
-  // initializers: {
-  //   todos: () => defaults.todos
-  // }
+  link: stateLink,
+  initializers: {
+    todos: () => getTodos()
+  }
 });
 
 // client.onResetStore(stateLink.writeDefaults);
