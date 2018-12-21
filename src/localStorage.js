c30cd548c8b904052688bb3052061892a5ee4bf4
Taesu Hyeon <qvil1127@gmail.com>
2018-12-21 23:31:51 +0900

    chrome storage not defined?

--- src/localStorage.js
+++ src/localStorage.js
@@ -2,15 +2,24 @@ import { GET_TODOS } from "./graphql/queries";
 
 export const saveTodos = cache => {
   const { todos } = cache.readQuery({ query: GET_TODOS });
+  const _todos = JSON.stringify(todos);
   try {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    // localStorage.setItem("todos", _todos);
+    chrome.storage.local.set({ todos: _todos }, () => {
+      console.log(`Set value : ${_todos}`);
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
 export const getTodos = () => {
-  const todos = localStorage.getItem("todos");
+  // const todos = localStorage.getItem("todos");
+  var todos;
+  chrome.storage.local.get(["todos"], result => {
+    todos = result;
+    console.log(`Get value : ${result}`);
+  });
   if (todos) {
     try {
       return JSON.parse(todos);
