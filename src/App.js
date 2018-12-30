c1abea0dfc03e0c085a58d29b693fcbc301a3f68
Taesu Hyeon <qvil1127@gmail.com>
2018-12-31 03:25:56 +0900

    Fixed bug : handling empty input (but have bug)
    * Refresh when return in onSubmit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
             <Title>Today Scrum</Title>
             <Form
               onSubmit={e => {
+                if (input.value.trim() === "") return;
                 e.preventDefault();
                 addTodo({ variables: { text: input.value } });
                 input.value = "";
